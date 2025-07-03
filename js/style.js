9a1e86174ac7751a04ad9f90d739d231fbbafae4
rohitgoyal345 <rhogoyal2005@gmail.com>
2025-07-03 15:24:12 +0530

    new update

--- js/style.js
+++ js/style.js
@@ -40,3 +40,30 @@ document.addEventListener('DOMContentLoaded', () => {
         animateCounters();
     });
 });
+
+
+
+
+
+const form = document.getElementById("contactForm");
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  const response = await fetch("https://formspree.io/f/mblybajn", {
+    method: "POST",
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    alert("Thank you! Your message has been sent.");
+    form.reset();
+  } else {
+    alert("Oops! There was a problem submitting your form.");
+  }
+});
